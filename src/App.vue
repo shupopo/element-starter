f6f203dc2dadf1e5d7efe551c4312e221bfb2c76
shupopo <saihatenoasa@gmail.com>
2019-02-12 21:40:09 +0900

    add some components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
+    <div id="app">
+        <div>
+            <el-button @click="startHacking">Start</el-button>
+            <el-input placeholder="Please input" v-model="input1"></el-input>
+            <el-time-select
+                    v-model="input2"
+                    :picker-options="{
+    start: '08:30',
+    step: '00:15',
+    end: '18:30'
+  }"
+                    placeholder="Select time">
+            </el-time-select>
+            <el-switch v-model="value1">
+            </el-switch>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
-    }
-  }
-}
+  export default {
+    data:function(){
+      return{
+        value1:""
+      }
+    },
+    methods: {
+      startHacking() {
+        this.$notify({
+          title: 'It works!',
+          type: 'success',
+          message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+          duration: 5000,
+        });
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
+    #app {
+        font-family: Helvetica, sans-serif;
+        text-align: center;
+    }
 </style>
